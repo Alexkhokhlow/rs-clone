7ae906c527a180d49f194e6684db9fb77d15ffd2
Alexkhokhlow <Lomtik25@gmail.com>
2023-02-02 14:13:37 +0300

    fix: fix routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
 import Home from './pages/home/home';
 import Login from './pages/login/login';
+import User from './pages/user/user';
 
 export default class App {
   home: Home;
   login: Login;
   body: HTMLElement;
+  user: User;
+
   constructor() {
     this.home = new Home();
     this.login = new Login();
+    this.user = new User();
     this.body = document.body;
   }
 
@@ -17,20 +21,30 @@ export default class App {
 
   openPage() {
     const path = window.location.pathname;
-    switch (path) {
-      case '/home': {
+    const routes = [/home/g, /login/g, /user\/([\w]+?)\b/g];
+    let flag = true;
+    routes.forEach((route) => {
+      const match = path.match(route);
+      if (match) {
+        flag = false;
+        if (match[0].includes('home')) {
+          this.body.append(this.home.home);
+          return;
+        }
+        if (match[0].includes('login')) {
+          this.body.append(this.login.login);
+          return;
+        }
+        if (match[0].includes('user')) {
+          this.body.append(this.user.user);
+          return;
+        }
+      }
+    });
+    if (flag) {
+      if (path === '/') {
         this.body.append(this.home.home);
-        break;
-      }
-      case '/login': {
-        this.body.append(this.login.login);
-        break;
-      }
-      case '/': {
-        this.body.append(this.home.home);
-        break;
-      }
-      default: {
+      } else {
         this.body.innerHTML = 'error';
       }
     }
