ec870f9ab80fc9bef00ecb257dfebbf823709ff1
Alexkhokhlow <Lomtik25@gmail.com>
2023-02-05 21:59:45 +0300

    fix: change value

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,28 +13,29 @@ export default class App {
     this.login = new Login();
     this.user = new User();
     this.body = document.body;
-   // const data = JSON.stringify({ email: 'ssfcs@gmail.com', password: 'dasdas', userName: 'sdvfnf' });
-    // fetch('http://localhost:8081/api/users/signup', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: data,
-    //   credentials:'same-origin'
-    // })
-    //   .then((response) => {
-    //     return response.text();
+    //  const data = JSON.stringify({ email: 'ssfcs@gmail.com', password: 'dasdas', userName: 'sdvfnf' });
+    //   fetch('http://localhost:8081/api/users/signup', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: data,
+    //     credentials:'same-origin'
     //   })
-    //   .then((text: string) => console.log(text))
-    //   .catch((err: Error) => console.error(err));
+    //     .then((response) => {
+    //       return response.text();
+    //     })
+    //     .then((text: string) => console.log(text))
+    //     .catch((err: Error) => console.error(err));
   }
 
   start() {
+    this.openPage();
   }
 
   openPage() {
     const path = window.location.pathname;
-    const routes = [/home/g, /login/g, /user\/([\w]+?)\b/g];
+    const routes = [/\/home\b/g, /\/login\b/g, /\/user\/([\w]+?)\b/g];
     let flag = true;
     routes.forEach((route) => {
       const match = path.match(route);
--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,6 @@ import Home from './app/pages/home/home';
 import Login from './app/pages/login/login';
 import User from './app/pages/user/user';
 
-const home = new Home();
-const login = new Login();
-const user = new User();
-const body = document.body;
 
-const handleLocation = () => {
-  const path = window.location.pathname;
-  const routes = [/home/g, /login/g, /user\/([\w]+?)\b/g];
-  let flag = true;
-  routes.forEach((route) => {
-    const match = path.match(route);
-    if (match) {
-      flag = false;
-      if (match[0].includes('home')) {
-        body.append(home.home);
-        return;
-      }
-      if (match[0].includes('login')) {
-        body.append(login.login);
-        return;
-      }
-      if (match[0].includes('user')) {
-        user.init(match[0]);
-        body.append(user.user);
-        return;
-      }
-    }
-  });
-  if (flag) {
-    if (path === '/') {
-      body.append(home.home);
-    } else {
-      body.innerHTML = 'error';
-    }
-  }
-};
-window.onpopstate = handleLocation;
-handleLocation();
+const app = new App();
+app.start();
