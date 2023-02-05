b3c0623890bfa0d0e500c26ec767888a92c39fbe
Alexkhokhlow <Lomtik25@gmail.com>
2023-02-05 22:16:30 +0300

    fix: remove test data

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,15 +1,8 @@
 export default class Home {
   home: HTMLElement;
-  button: HTMLButtonElement;
 
   constructor() {
     this.home = document.createElement('section');
     this.home.classList.add('dsa');
-    this.button = document.createElement('button');
-    this.button.innerHTML = 'buuton';
-    this.home.append(this.button);
-    this.button.addEventListener('click', () => {
-      window.location.href = 'login';
-    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import './scss/global.scss';
 import App from './app/app';
-import Home from './app/pages/home/home';
-import Login from './app/pages/login/login';
-import User from './app/pages/user/user';
-
 
 const app = new App();
 app.start();
