960ed5b263f4e6b33423f5d37b46622923618215
Maria <sheyko.mari@mail.ru>
2023-02-19 19:14:56 +0300

    feat: add interaction icon and some styles

--- src/app/pages/board/tasksList/task/task.ts
+++ src/app/pages/board/tasksList/task/task.ts
@@ -8,14 +8,17 @@ export default class Task {
 
   priority: Priority;
 
+  interaction: HTMLElement;
+
   constructor(title: string, onClick: (event: Event) => void, listName: string, index: string, id: string) {
     this.task = Common.createDOMNode('div', ['task']);
     this.task.draggable = true;
     this.title = Common.createDOMNode('span', ['task__title'], title);
+    this.interaction = Common.createDOMNode('div', ['task__interaction']);
     this.priority = new Priority();
     this.task.setAttribute('data-title', title);
     this.task.setAttribute('data-id', id);
     this.task.addEventListener('click', onClick);
-    this.task.append(this.title, this.priority.priority);
+    this.task.append(this.title, this.priority.priority, this.interaction);
   }
 }
