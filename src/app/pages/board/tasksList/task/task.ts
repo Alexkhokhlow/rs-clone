e3c8c37522edf90d4a6b2ba723cc56c197c074a9
Maria <sheyko.mari@mail.ru>
2023-02-20 23:54:47 +0300

    feat: add backdrop

--- src/app/pages/board/tasksList/task/task.ts
+++ src/app/pages/board/tasksList/task/task.ts
@@ -10,6 +10,8 @@ export default class Task {
 
   interaction: HTMLElement;
 
+  backdrop: HTMLElement;
+
   constructor(title: string, onClick: (event: Event) => void, listName: string, index: string, id: string) {
     this.task = Common.createDOMNode('div', ['task']);
     this.task.draggable = true;
@@ -20,6 +22,7 @@ export default class Task {
     this.task.setAttribute('data-id', id);
     this.task.addEventListener('click', onClick);
     this.task.append(this.title, this.priority.priority, this.interaction);
+    this.backdrop = Common.createDOMNode('div', ['backdrop']);
 
     this.addHandlers();
   }
@@ -27,6 +30,19 @@ export default class Task {
   private addHandlers() {
     this.interaction.addEventListener('click', (e) => {
       e.stopPropagation();
+      const selectedTask = (e.target as HTMLElement).closest('.task');
+      if (selectedTask?.classList.contains('task_selected')) {
+        this.backdrop.remove();
+      } else {
+        document.body.append(this.backdrop);
+      }
+
+      selectedTask?.classList.toggle('task_selected');
+
+      this.backdrop.addEventListener('click', () => {
+        this.backdrop.remove();
+        selectedTask?.classList.remove('task_selected');
+      });
       // TODO: открыть модальное окно
     });
   }
---
+++ src/app/pages/board/tasksList/task/taskInteraction.ts
@@ -0,0 +1,16 @@
+// import Common from '../../../../utils/common';
+
+// export default class TaskModal {
+//   constructor() {
+//     this.modal
+
+//     this.render();
+//   }
+
+//   public render() {
+//     this.addHandlers();
+//     return this.modal;
+//   }
+
+//   addHandlers() {}
+// }
