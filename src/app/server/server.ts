7133d327bb78ed816a9c714caf4dea887af7ee63
Alexkhokhlow <Lomtik25@gmail.com>
2023-02-27 12:00:33 +0300

    fix: fix link

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -3,9 +3,8 @@ import { ITaskInfo } from '../../types/types';
 
 export default class Server {
   private address: string;
-  //https://trello-clone-x3tl.onrender.com
   constructor() {
-    this.address = 'http://localhost:3000/api';
+    this.address = 'https://trello-clone-x3tl.onrender.com/api';
   }
 
   async checkToken(token: string) {
